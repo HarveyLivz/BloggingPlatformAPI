a046b229ba4e8aea1cca3edf6dda0690beb4c1ca
HarveyLivz <itsharvey2002@gmail.com>
2025-02-18 21:18:56 +0330

    todos

--- Api/Services/Interfaces/IPostService.cs
+++ Api/Services/Interfaces/IPostService.cs
@@ -4,7 +4,16 @@ namespace Api.Services.Interfaces
 {
     public interface IPostService
     {
+        // TODO: Create, Update, Delete Post
+        // TODO: Get All Posts by User ID
+        // TODO: Get Post by Post ID
+        // TODO: Get All Posts
+        
         Task<CreatePostResponseDto?> CreatePost(CreatePostRequestDto createPostRequestDto, Guid userId);
-        Task<CreatePostResponseDto?> GetPostById(Guid userId);
+        Task<CreatePostRequestDto> DeletePostById(Guid postId);
+        Task<CreatePostResponseDto?> GetAllPostsByUserId(Guid userId);
+        Task<CreatePostRequestDto> GetPostByPostId(Guid postId);
+
+
     }
 }
